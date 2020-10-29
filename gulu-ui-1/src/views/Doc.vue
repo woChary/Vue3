4dd96d76a6679681352219ad85dfbe0536851997
woChary <15521223007@163.com>
2020-10-29 20:15:47 +0800

    初始化aside

--- gulu-ui-1/src/views/Doc.vue
+++ gulu-ui-1/src/views/Doc.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
-        <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
-        </div>
+        <Topnav />
         <div class="content">
-            <aside>aside</aside>
-            <main>content</main>
-          
+            <aside>边栏</aside>
+            <aside>
+                <h2>组件列表</h2>
+                <ol>
+                    <li>
+                        <router-link to="/doc/swich">Switch 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/button">Button 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/dialog">Dialog 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/tabs">Tabs 组件</router-link>
+                    </li>
+                </ol>
+            </aside>
+            <main>主内容</main>
         </div>
     </div>
 </template>
+<script lang="ts">
+    import Topnav from '../components/Topnav.vue'
+    export default {
+        components: { Topnav }
+    }
+</script>
